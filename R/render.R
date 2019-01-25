5475038be4957df23aa597271689a461d3378a29
Inés Añibarro <39153852+inesani@users.noreply.github.com>
2019-01-25 13:18:17 +0100

    to html

--- R/render.R
+++ R/render.R
@@ -9,7 +9,7 @@ render <- function(dataframe, dimensions) {
   rmarkdown::render("/usr/local/lib/opencpu/apps/ocpu_github_inesani_stan/stanopencpu/stan.Rmd",
 		    #output_file="output.html",
                     output_dir=getwd(),
-                    intermediates_dir=getwd(), params=list(test=dataframe), output_format=("html_document", "pdf_document"));
+                    intermediates_dir=getwd(), params=list(test=dataframe), output_format="html_document");
   invisible();
 }
 
