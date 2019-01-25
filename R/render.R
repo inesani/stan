8aed99bf549d6de04b70d4c04d0c7d4c186df952
Inés Añibarro <39153852+inesani@users.noreply.github.com>
2019-01-25 08:23:46 +0100

    Force change to deploy OpenCpu

--- R/render.R
+++ R/render.R
@@ -5,7 +5,7 @@ render <- function(dataframe, dimensions) {
   # Alternative source of data for testing
   #dataframe <-read.csv("/usr/local/lib/R/site-library/stan-opencpu/series1.txt", header = TRUE)
 
-	# Render the markdwn
+	# Render the markdown
   rmarkdown::render("/usr/local/lib/opencpu/apps/ocpu_github_inesani_stan/stanopencpu/stan.Rmd", output_file="output.html",
                     output_dir=getwd(),
                     intermediates_dir=getwd(), params=list(test=dataframe), output_format="html_document");
