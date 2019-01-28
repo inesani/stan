51a5e508d373555376b466d12bfd14756738b083
Inés Añibarro <39153852+inesani@users.noreply.github.com>
2019-01-28 15:49:56 +0100

    html

--- R/render.R
+++ R/render.R
@@ -8,7 +8,7 @@ render <- function(dataframe, dimensions) {
 	# Render the markdown
   rmarkdown::render(
 	  	    "/usr/local/lib/opencpu/apps/ocpu_github_inesani_stan/stanopencpu/stan.Rmd",
-		    output_format = 'all',
+		    output_format = 'html_document',
 		    params=list(test=dataframe),
                     output_dir=getwd(),
                     intermediates_dir=getwd()
